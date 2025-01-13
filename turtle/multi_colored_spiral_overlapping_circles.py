a28882ad5fbb3a677bd060176d5517601cf7c9f4
Mihai Apostol <mihaiapostol153@gmail.com>
2025-01-13 09:22:43 +0200

    added new figure multi colored spiral of overlapping circles.

---
+++ turtle/multi_colored_spiral_overlapping_circles.py
@@ -0,0 +1,17 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("black")
+
+my_turtle = turtle.Turtle()
+my_turtle.speed(0)
+
+colors = ["blue", "white", "orange", "pink"]
+
+for _ in range(36):
+    my_turtle.pencolor(colors[_ % 4])
+    my_turtle.circle(100)
+    my_turtle.left(10)
+
+my_turtle.hideturtle()
+screen.exitonclick()
