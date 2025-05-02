3131326805701b652d08cfb9e86b1b26991246de
Mihai Apostol <mihaiapostol153@gmail.com>
2025-05-02 10:46:46 +0300

    Moving code into function

--- turtle/multi_colored_spiral_overlapping_circles.py
+++ turtle/multi_colored_spiral_overlapping_circles.py
@@ -1,17 +1,22 @@
 import turtle
 
-screen = turtle.Screen()
-screen.bgcolor("black")
 
-my_turtle = turtle.Turtle()
-my_turtle.speed(0)
+def multi_colored_spiral_overlapping_circles():
+    screen = turtle.Screen()
+    screen.bgcolor("black")
 
-colors = ["blue", "white", "orange", "pink"]
+    my_turtle = turtle.Turtle()
+    my_turtle.speed(0)
 
-for _ in range(36):
-    my_turtle.pencolor(colors[_ % 4])
-    my_turtle.circle(100)
-    my_turtle.left(10)
+    colors = ["blue", "white", "orange", "pink"]
 
-my_turtle.hideturtle()
-screen.exitonclick()
+    for _ in range(36):
+        my_turtle.pencolor(colors[_ % 4])
+        my_turtle.circle(100)
+        my_turtle.left(10)
+
+    my_turtle.hideturtle()
+    screen.exitonclick()
+
+
+multi_colored_spiral_overlapping_circles()
