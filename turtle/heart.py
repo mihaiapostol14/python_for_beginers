67c7788fb6438256e65dd4487f46aaaee70d18f5
Mihai Apostol <mihaiapostol153@gmail.com>
2025-05-02 10:22:05 +0300

    Changed project structure from simple functions to class with methods

--- turtle/heart.py
+++ turtle/heart.py
@@ -2,19 +2,23 @@ import math
 from turtle import *
 
 
-def hearta(k):
-    return 15 * math.sin(k) ** 3
+class Heart:
+    def __init__(self):
+        self.run()
 
+    def hearta(self,k):
+        return 15 * math.sin(k) ** 3
 
-def heartb(k):
-    return 12 * math.cos(k) - 5 * math.cos(2 * k) - 2 * math.cos(3 * k) - math.cos(4 * k)
+    def heartb(self,k):
+        return 12 * math.cos(k) - 5 * math.cos(2 * k) - 2 * math.cos(3 * k) - math.cos(4 * k)
 
-
-speed(0)
-bgcolor("black")
-for i in range(6000):
-    goto(hearta(i) * 20, heartb(i) * 20)
-    for j in range(5):
-        color("red")
-        goto(0, 0)
-done()
+    def run(self):
+        speed(0)
+        bgcolor("black")
+        for i in range(6000):
+            goto(self.hearta(i) * 20, self.heartb(i) * 20)
+            for j in range(5):
+                color("red")
+                goto(0, 0)
+        done()
+Heart()
