66a844ea0106969d95b7bdba80c68c137feab510
Mihai Apostol <mihaiapostol153@gmail.com>
2025-05-02 10:46:46 +0300

    Moving code into function

--- turtle/color_flower.py
+++ turtle/color_flower.py
@@ -1,21 +1,26 @@
 from turtle import *
 from colorsys import *
 
-tracer(7)
 
-bgcolor('black')
-for j in range(36):
-    h = j / 36
+def color_flower():
+    tracer(7)
 
-    color(hsv_to_rgb(h, 0.8, 1))
+    bgcolor('black')
+    for j in range(36):
+        h = j / 36
 
-    begin_fill()
+        color(hsv_to_rgb(h, 0.8, 1))
 
-    for i in range(2):
-        circle(140, 90)
-        left(90)
-        circle(140, 90)
-        left(90)
-    end_fill()
-    right(10)
-done()
+        begin_fill()
+
+        for i in range(2):
+            circle(140, 90)
+            left(90)
+            circle(140, 90)
+            left(90)
+        end_fill()
+        right(10)
+    done()
+
+
+color_flower()
