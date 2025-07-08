93640f19e6182f5b04d311892a8d73c5b5ce8785
Mihai Apostol <mihaiapostol153@gmail.com>
2025-07-08 14:09:03 +0300

    added split_by_divisibility.py

---
+++ algorithms/split_by_divisibility.py
@@ -0,0 +1,9 @@
+def split_by_divisibility(array):
+    number_1 = [number1 for number1 in array if number1 % 2 == 0 and number1 % 4 == 0]
+    number_2 = [number2 for number2 in array if number2 % 2 != 0 and number2 % 4 != 0]
+
+    print(f"this is divide with 2: {number_1}")
+    print(f"this is not divide with 2: {number_2}")
+
+
+split_by_divisibility(array=tuple(range(1,10)))
